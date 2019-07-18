0167fee45e9074c8975aa37347fef4e23db3bedc
Ziya Mollamahmut <ziyadmm@gmail.com>
2019-07-18 12:35:56 +0300

    added LazZiya.TagHelpers.PagingTagHelper

---
+++ PagingWithDB/PagingWithDB/Models/ItemsListBase.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PagingWithDB.Models
+{
+    /// <summary>
+    /// this is a generic base class that can be derivered to create
+    /// list of paginated items for any other type of classes.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class ItemsListBase<T> where T : class
+    {
+        public int PageNo { get; set; } = 1;
+        public int PageSize { get; set; } = 10;
+        public int TotalRecords { get; set; } = 0;
+        public ICollection<T> Items { get; set; }
+    }
+}
---
+++ PagingWithDB/PagingWithDB/Models/QuestionsList.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PagingWithDB.Models
+{
+    // questions list is inherited from ItemsListBase class
+    public class QuestionsList : ItemsListBase<Questions>
+    {
+    }
+}
